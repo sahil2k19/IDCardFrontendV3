5960f51a22c2d4f695facd0fcdcc1fd9e00efa73
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-06-16 13:08:09 +0530

    push

--- src/Component/Main/IdCard/IdCard.jsx
+++ src/Component/Main/IdCard/IdCard.jsx
@@ -279,7 +279,7 @@ const defaultElementStyles = {
                   fontSize: `${styles.name.fontSize}px`,
                   color: styles.name.color,
                 }}
-                className="absolute top-[200px] uppercase  transform -translate-x-1/2 text-[20px] font-bold   mt-2 w-full text-white"
+                className="absolute bottom-[130px]  text-lg transform -translate-x-1/2 font-bold whitespace-nowrap text-center text-white mt-1  px-10"
               >
                   {card.firstName}
               </p>
@@ -294,7 +294,7 @@ const defaultElementStyles = {
                   fontSize: `${styles.institute.fontSize}px`,
                   color: styles.institute.color,
                 }}
-                className="absolute bottom-[130px]  text-lg transform -translate-x-1/2 font-semibold   text-white mt-1  px-10"
+                className="absolute bottom-[130px]  text-lg transform -translate-x-1/2 font-semibold text-center text-white mt-1  px-10"
                 dangerouslySetInnerHTML={{
                   __html: card.institute.toUpperCase(),
                 }}
@@ -310,7 +310,7 @@ const defaultElementStyles = {
                   fontSize: `${styles.designation.fontSize}px`,
                   color: styles.designation.color,
                 }}
-                className="absolute bottom-[107px]  transform -translate-x-1/2 text-md font-bold   text-black"
+                className="absolute bottom-[107px]  transform -translate-x-1/2 text-md font-bold text-center text-black"
               >
                 {card.designation}
               </p>
@@ -339,7 +339,7 @@ const defaultElementStyles = {
                   fontSize: `${styles.participantId.fontSize}px`,
                   color: styles.participantId.color,
                 }}
-                className="absolute bottom-[1px]  transform -translate-x-1/2 text-xs font-bold   text-black"
+                className="absolute bottom-[1px]  transform -translate-x-1/2 text-xs font-bold text-center text-black"
               >
                 {card.participantId}
               </div>
